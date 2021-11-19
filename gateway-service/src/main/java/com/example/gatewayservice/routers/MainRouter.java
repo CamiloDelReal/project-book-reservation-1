1f2a952a8db0c1e3c699d3e4aee0e9ebe7c8ace5
Camilo del Real <cjdelreal2009@gmail.com>
2021-11-19 15:39:05 +0000

    ADD: Gateway service

--- gateway-service/src/main/java/com/example/gatewayservice/routers/MainRouter.java
+++ gateway-service/src/main/java/com/example/gatewayservice/routers/MainRouter.java
@@ -1,4 +1,27 @@
 package com.example.gatewayservice.routers;
 
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+
+@Configuration
 public class MainRouter {
+
+    @Bean
+    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r
+                        .path("/users/**")
+                        .and()
+                        .method(HttpMethod.GET, HttpMethod.POST, HttpMethod.PUT, HttpMethod.DELETE)
+                        .filters(f -> f
+                                .rewritePath("/(?<segment>.*)", "/${segment}")
+                        )
+                        .uri("lb://users-service")
+                )
+                .build();
+    }
+
 }
