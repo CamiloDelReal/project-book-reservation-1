5e89310c6687474e14479c2767db330a301b7769
Camilo del Real <cjdelreal2009@gmail.com>
2021-11-19 15:38:46 +0000

    ADD: Discovery service

--- discovery-service/src/main/java/com/example/discoveryservice/security/WebSecurity.java
+++ discovery-service/src/main/java/com/example/discoveryservice/security/WebSecurity.java
@@ -1,4 +1,20 @@
 package com.example.discoveryservice.security;
 
-public class WebSecurity {
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().disable();  // We should normally lock this service to everyone except for the admin source
+        http.csrf().disable();
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+
 }
